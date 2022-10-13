faed3c8995728c51e59858177b571fa94aaa5a8c
Mathilde Gamore <malega89@hotmail.com>
2022-10-13 11:51:37 +0200

    2ememodifConnection

--- src/views/ConnectionView.vue
+++ src/views/ConnectionView.vue
@@ -1,6 +1,30 @@
 <template>
-    <div class="connexion">
-      <h1>This connexion vue</h1>
-      <input type="text">Connectez vous
+    <div class="formconnexion">
+      <form action="">
+            <div class="formConnex">
+              <h1>Connexion</h1>
+              <input type="text" placeholder="Identifiant" id="inputIdentifiant">        
+              <input type="text" placeholder="Mot de passe" id="inputMDP">
+              <input type="submit" value="Se connecter">
+              
+            </div>
+        </form>
     </div>
-  </template>
+</template>
+
+<style lang="scss" scoped>
+
+$colorPrimary:#A52A2A;
+.formConnex{
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+}
+input{
+  margin: 10px;
+  padding: 10px 20px;
+}
+h1{
+  color:$colorPrimary;
+}
+</style>
