d4e0ac0e417640fae3537f6fd18f44d1f9fc9a41
nabil <amr.nabz@gmail.com>
2022-10-14 12:26:56 +0200

    ajout de composants nvbar & homecontent

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,17 +9,5 @@
         <RouterLink class="routerLink" to="/connexion">Connexion</RouterLink>
       </nav>
     </div>
-    
   </header>
-  <div>
-    <div class="image-main">
-      <img src="../src/img/logo.png">
-      <h1>VOTRE RESEAU SOCIAL CULINAIRE PREFERE</h1>
-    </div>
-  </div>
-
-  <RouterView />
-  <div class="cardContent">
-    
-  </div>
 </template>
