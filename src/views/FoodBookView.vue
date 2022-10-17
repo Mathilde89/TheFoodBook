a53d72ecfa420f3259959a73bf15475bdff31337
Mathilde Gamore <malega89@hotmail.com>
2022-10-17 13:57:33 +0200

    local storage

--- src/views/FoodBookView.vue
+++ src/views/FoodBookView.vue
@@ -20,19 +20,20 @@ export default {
       const response = await fetch("https://social-network-api.osc-fr1.scalingo.io/foodbook/posts?page=0&limit=10", options);
       const data = await response.json();
       console.log("data", data)
-
-
-
     },
 
      // Requete pour créer un post
     CreatePost: async function () {
 
+      //Reccupère le token qui est dans le local storage
+      let token= JSON.parse(localStorage.getItem("tokenUserLog"));
+      console.log("token", token)
+
       const options = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzRkMDEyNWNjYWY3ZjAwMWQ5MGRhYWYiLCJpYXQiOjE2NjU5OTU5NDEsImV4cCI6MTY2NjA4MjM0MX0.FSGhUxpn0R1vvNLvUH3WKoPCSN1szF58Nt3cUTVpmyM"
+          "Authorization": `bearer ${token}`
         },
         body: JSON.stringify({
           title: "tittletest",
@@ -51,49 +52,49 @@ export default {
   },
 };
 // //Fait pour pouvoir tester à enlever après le fetch
-// const testposterecette1 = {
-//   auteur1: "Jane Doe",
-//   titre1: "Couscous marocain",
-//   recette1:
-//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore cumque quaerat ea laborum quod minima. Quisquam dolores sapiente, facilis deserunt minima sed recusandae itaque ducimus alias quos odit culpa debitis.",
-//   date1: "13/10/2022",
-//   heure1: "15:40",
-//   nblike1: 10,
-//   nbcom1: "30",
-//   id1: "test1",
-//   post1a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//   post1b:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//   post1c:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+const testposterecette1 = {
+  auteur1: "Jane Doe",
+  titre1: "Couscous marocain",
+  recette1:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore cumque quaerat ea laborum quod minima. Quisquam dolores sapiente, facilis deserunt minima sed recusandae itaque ducimus alias quos odit culpa debitis.",
+  date1: "13/10/2022",
+  heure1: "15:40",
+  nblike1: 10,
+  nbcom1: "30",
+  id1: "test1",
+  post1a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  post1b:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  post1c:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 
-// };
-// const testposterecette2 = {
-//   auteur2: "Titin Milou",
-//   titre2: "Pizza",
-//   recette2:
-//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore cumque quaerat ea laborum quod minima. Quisquam dolores sapiente, facilis deserunt minima sed recusandae itaque ducimus alias quos odit culpa debitis.",
-//   date2: "04/10/2022",
-//   heure2: "09:23",
-//   nblike2: 1,
-//   nbcom2: "1",
-//   id2: "test2",
-//   post2a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//   post2b:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//   post2c:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-// };
-// const testposterecette3 = {
-//   auteur3: "Juste Leblanc",
-//   titre3: "Quiche",
-//   recette3:
-//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore cumque quaerat ea laborum quod minima. Quisquam dolores sapiente, facilis deserunt minima sed recusandae itaque ducimus alias quos odit culpa debitis.",
-//   date3: "28/01/2013",
-//   heure3: "09:33",
-//   nblike3: 42,
-//   nbcom3: "20",
-//   id3: "test3",
-//   post3a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//   post3b:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//   post3c:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-// };
+};
+const testposterecette2 = {
+  auteur2: "Titin Milou",
+  titre2: "Pizza",
+  recette2:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore cumque quaerat ea laborum quod minima. Quisquam dolores sapiente, facilis deserunt minima sed recusandae itaque ducimus alias quos odit culpa debitis.",
+  date2: "04/10/2022",
+  heure2: "09:23",
+  nblike2: 1,
+  nbcom2: "1",
+  id2: "test2",
+  post2a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  post2b:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  post2c:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+};
+const testposterecette3 = {
+  auteur3: "Juste Leblanc",
+  titre3: "Quiche",
+  recette3:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore cumque quaerat ea laborum quod minima. Quisquam dolores sapiente, facilis deserunt minima sed recusandae itaque ducimus alias quos odit culpa debitis.",
+  date3: "28/01/2013",
+  heure3: "09:33",
+  nblike3: 42,
+  nbcom3: "20",
+  id3: "test3",
+  post3a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  post3b:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  post3c:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+};
 
 </script>
 
@@ -133,8 +134,10 @@ export default {
     :id="testposterecette3.id3"
     :listcompost="testposterecette3.post3a"
   />
-  <form @submit.prevent="CreatePost()" action="">
 
+ <!-- pour test à enlever après -->
+  <form @submit.prevent="CreatePost()" action="">
+    
     <button type="submit"></button>
   </form>
 
